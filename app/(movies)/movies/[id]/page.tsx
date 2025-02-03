34ccdfdf32bba173bee0865dbb17df96e7f6394e
LJunsu <tpwotpwo13@gmail.com>
2025-02-03 13:54:54 +0900

    3.5 Deployment

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,6 +1,13 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovies } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
+
+export async function generateMetadata({params}: Props) {
+    const movie = await getMovies(params.id);
+    return {
+        title: movie.title
+    }
+}
 
 type Props = {
     params: {id: string};
@@ -10,9 +17,7 @@ export default async function MovieDetail({params}: Props) {
     const { id } = params;
 
     return (
-        <div>
-            <h3>Movie detail page </h3>
-             
+        <div>             
             <Suspense fallback={<h1>Loading movie info</h1>}>
                 <MovieInfo id={id} />
             </Suspense>
--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,8 +1,9 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-info.module.css";
 
-async function getMovies(id: string) {
+export async function getMovies(id: string) {
     console.log(`Fetching movies : ${Date.now()}`);
-    await new Promise((resolve) => setTimeout(resolve, 6000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     
     const response = await fetch(`${API_URL}/${id}`);
     return  response.json();
@@ -12,5 +13,25 @@ type Props = {id: string}
 export default async function MovieInfo({id}: Props) {
     const movie = await getMovies(id);
 
-    return <h6>{JSON.stringify(movie)}</h6>
+    return(
+        <div className={styles.container}>
+            <img
+                className={styles.poster}
+                src={movie.poster_path}
+                alt={movie.title}
+            />
+            
+            <div className={styles.info}>
+                <h1 className={styles.title}>{movie.title}</h1>
+                <h3>⭐{movie.vote_average.toFixed(1)}</h3>
+                <p>{movie.overview}</p>
+                <a 
+                    href={movie.homepage}
+                    target={"_blank"}
+                >
+                    Homepage &rarr;
+                </a>
+            </div>
+        </div>
+    )
 }
